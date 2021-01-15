f4cce0c56adc8d0b45f8d044b16d1091adb4b224
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-01-15 16:20:33 -0700

    Add function to calculate total amount spent on trips

---
+++ src/destination.js
@@ -0,0 +1,16 @@
+class Destination {
+  constructor(id, destination, estimatedLodgingCostPerDay, estimatedFlightCostPerPerson, image, alt) {
+      this.id = id;
+      this.destination = destination;
+      this.estimatedLodgingCostPerDay = estimatedLodgingCostPerDay;
+      this.estimatedFlightCostPerPerson = estimatedFlightCostPerPerson;
+      this.image = image;
+      this.alt = alt;
+  }
+
+  // calculateEstimatedTripCost() {
+      
+  // }
+}
+
+export default Destination;
