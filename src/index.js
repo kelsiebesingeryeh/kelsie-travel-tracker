cc6e9dc61126a825caab234b22c31c58246e0f41
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-01-17 15:33:18 -0700

    Add functionality for current trips to display:

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './css/base.scss';
 import Trip from './trip';
 import Traveler from './traveler';
 import ApiCall from './apiCalls';
+import domUpdates from './domUpdates';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
@@ -27,15 +28,20 @@ let pastTrips = document.querySelector(".past-trips");
 let pastTripsArea = document.querySelector(".past-trips-area");
 let pastTripsText = document.querySelector(".past-trips-text");
 
+let currentTrips = document.querySelector(".current-trips");
+let currentTripsArea = document.querySelector(".current-trips-area");
+let currentTripsText = document.querySelector(".current-trips-text");
+
 let travelerApi;
 let destinationApi;
 let tripApi;
 let traveler;
 
 window.onload = getAllData();
-pendingTrips.addEventListener('click', displayPendingTrips);
-upcomingTrips.addEventListener('click', displayUpcomingTrips);
-pastTrips.addEventListener('click', displayPastTrips);
+pendingTrips.addEventListener("click", displayPendingTrips);
+upcomingTrips.addEventListener("click", displayUpcomingTrips);
+pastTrips.addEventListener("click", displayPastTrips);
+currentTrips.addEventListener('click', displayCurrentTrips)
 // homeButton.addEventListener("click", returnHome);
 
 function getAllData() {
@@ -72,10 +78,10 @@ function createTravelerProfile(travelerInfo, tripInfo, destinationInfo) {
  traveler = new Traveler(newTraveler, tripInfo, destinationInfo);
 }
 
-function displayTrips(tripsList) {
-    tripsArea.innerHTML = '';
-    tripsList.trips.forEach(trip => {
-        let tripsHTML = `
+ function displayTrips(tripsList) {
+        tripsArea.innerHTML = '';
+        tripsList.trips.forEach(trip => {
+            let tripsHTML = `
         <div class='info-card'>
             <div class="image-styling">
                 <img src="${trip.destination.image}" alt="${trip.destination.alt}" class="trip-image">
@@ -85,25 +91,24 @@ function displayTrips(tripsList) {
             <p id="${trip.duration}-duration" class="trip-duration">Trip Duration: ${trip.duration}</p>
             <p id="${trip.travelers}-travelers" class="trip-travelers">Number of Travelers: ${trip.travelers}</p>
             <p id="${trip.status}-status" class="trip-status">Trip Status: ${trip.status}</p>
-        </div>
-    `;
-    tripsArea.insertAdjacentHTML('beforeend', tripsHTML)
-    })
-}
+        </div>`;
+            tripsArea.insertAdjacentHTML('beforeend', tripsHTML)
+        })
+    }
 
-function displayPendingTrips() {
-  tripsArea.classList.add('hidden');
-  pendingTripsArea.classList.remove("hidden");
-  allTripsText.classList.add('hidden');
-  yearCost.classList.add('hidden');
-  let pendingTripsList = traveler.getPendingTrips();
+    function displayPendingTrips() {
+        tripsArea.classList.add('hidden');
+        pendingTripsArea.classList.remove("hidden");
+        allTripsText.classList.add('hidden');
+        yearCost.classList.add('hidden');
+        let pendingTripsList = traveler.getPendingTrips();
 
-   if (pendingTripsList.length === 0) {
-       pendingTripsText.innerText = 'You Have No Pending Trips!'
-   } else {
-       pendingTripsArea.innerHTML = '';
-       pendingTripsList.forEach(trip => {
-           let pendingTripsHTML = `
+        if (pendingTripsList.length === 0) {
+            pendingTripsText.innerText = 'You Have No Pending Trips!'
+        } else {
+            pendingTripsArea.innerHTML = '';
+            pendingTripsList.forEach(trip => {
+                let pendingTripsHTML = `
            <div class='info-card'>
                <div class="image-styling">
                    <img src="${trip.destination.image}" alt="${trip.destination.alt}" class="trip-image">
@@ -115,23 +120,24 @@ function displayPendingTrips() {
                <p id="${trip.status}-status" class="trip-status">Trip Status: ${trip.status}</p>
            </div>
        `;
-       pendingTripsArea.insertAdjacentHTML('beforeend', pendingTripsHTML)
-   })
-}}
+                pendingTripsArea.insertAdjacentHTML('beforeend', pendingTripsHTML)
+            })
+        }
+    }
 
-function displayUpcomingTrips() {
-  tripsArea.classList.add("hidden");
-  upcomingTripsArea.classList.remove("hidden");
-  allTripsText.classList.add("hidden");
-  yearCost.classList.add("hidden");
-  let upcomingTripsList = traveler.getUpcomingTrips();
+    function displayUpcomingTrips() {
+        tripsArea.classList.add("hidden");
+        upcomingTripsArea.classList.remove("hidden");
+        allTripsText.classList.add("hidden");
+        yearCost.classList.add("hidden");
+        let upcomingTripsList = traveler.getUpcomingTrips();
 
-  if (upcomingTripsList.length === 0) {
-    upcomingTripsText.innerText = "You Have No Upcoming Trips!";
-  } else {
-    upcomingTripsArea.innerHTML = "";
-    upcomingTripsList.forEach((trip) => {
-      let upcomingTripsHTML = `
+        if (upcomingTripsList.length === 0) {
+            upcomingTripsText.innerText = "You Have No Upcoming Trips!";
+        } else {
+            upcomingTripsArea.innerHTML = "";
+            upcomingTripsList.forEach((trip) => {
+                let upcomingTripsHTML = `
            <div class='info-card'>
                <div class="image-styling">
                    <img src="${trip.destination.image}" alt="${trip.destination.alt}" class="trip-image">
@@ -143,24 +149,24 @@ function displayUpcomingTrips() {
                <p id="${trip.status}-status" class="trip-status">Trip Status: ${trip.status}</p>
            </div>
        `;
-      upcomingTripsArea.insertAdjacentHTML("beforeend", upcomingTripsHTML);
-    });
-  }
-}
+                upcomingTripsArea.insertAdjacentHTML("beforeend", upcomingTripsHTML);
+            });
+        }
+    }
 
-function displayPastTrips() {
-  tripsArea.classList.add("hidden");
-  pastTripsArea.classList.remove("hidden");
-  allTripsText.classList.add("hidden");
-  yearCost.classList.add("hidden");
-  let pastTripsList = traveler.getPastTrips();
+    function displayPastTrips() {
+        tripsArea.classList.add("hidden");
+        pastTripsArea.classList.remove("hidden");
+        allTripsText.classList.add("hidden");
+        yearCost.classList.add("hidden");
+        let pastTripsList = traveler.getPastTrips();
 
-  if (pastTripsList.length === 0) {
-    pastTripsText.innerText = "You Have No Upcoming Trips!";
-  } else {
-    pastTripsArea.innerHTML = "";
-    pastTripsList.forEach((trip) => {
-      let pastTripsHTML = `
+        if (pastTripsList.length === 0) {
+            pastTripsText.innerText = "You Have No Past Trips!";
+        } else {
+            pastTripsArea.innerHTML = "";
+            pastTripsList.forEach((trip) => {
+                let pastTripsHTML = `
            <div class='info-card'>
                <div class="image-styling">
                    <img src="${trip.destination.image}" alt="${trip.destination.alt}" class="trip-image">
@@ -170,15 +176,39 @@ function displayPastTrips() {
                <p id="${trip.duration}-duration" class="trip-duration">Trip Duration: ${trip.duration}</p>
                <p id="${trip.travelers}-travelers" class="trip-travelers">Number of Travelers: ${trip.travelers}</p>
                <p id="${trip.status}-status" class="trip-status">Trip Status: ${trip.status}</p>
-           </div>
-       `;
-      pastTripsArea.insertAdjacentHTML("beforeend", pastTripsHTML);
-    });
-  }
-}
+           </div>`;
+                pastTripsArea.insertAdjacentHTML("beforeend", pastTripsHTML);
+            });
+        }
+    }
 
+     function displayCurrentTrips() {
+       tripsArea.classList.add("hidden");
+       currentTripsArea.classList.remove("hidden");
+       allTripsText.classList.add("hidden");
+       yearCost.classList.add("hidden");
+       let currentTripsList = traveler.getCurrentTrips();
 
-
+       if (currentTripsList.length === 0) {
+         currentTripsText.innerText = "You Have No Current Trips!";
+       } else {
+         currentTripsArea.innerHTML = "";
+         currentTripsList.forEach((trip) => {
+           let currentTripsHTML = `
+           <div class='info-card'>
+               <div class="image-styling">
+                   <img src="${trip.destination.image}" alt="${trip.destination.alt}" class="trip-image">
+               </div>
+               <p id="${trip.destination.destination}-destination" class="trip-date">Destination: ${trip.destination.destination}</p>
+               <p id="${trip.date}-date" class="trip-date">Trip Date: ${trip.date}</p>
+               <p id="${trip.duration}-duration" class="trip-duration">Trip Duration: ${trip.duration}</p>
+               <p id="${trip.travelers}-travelers" class="trip-travelers">Number of Travelers: ${trip.travelers}</p>
+               <p id="${trip.status}-status" class="trip-status">Trip Status: ${trip.status}</p>
+           </div>`;
+           currentTripsArea.insertAdjacentHTML("beforeend", currentTripsHTML);
+         });
+       }
+     }
 
 
 
