96b184d77888d70f18a5aa3f0db83d24801801e0
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-01-17 15:04:11 -0700

    Add functionality to display past, pending and upcoming trips

--- src/index.js
+++ src/index.js
@@ -12,18 +12,31 @@ import './images/turing-logo.png'
 
 let tripsArea = document.querySelector('.travel-card-container');
 let yearCost = document.querySelector('.year-cost')
+let allTripsText = document.querySelector(".all-trips");
 
-let travelerApi,
-  destinationApi,
-  tripApi,
-  trip,
-  destination,
-  traveler,
-  userTrip,
-  userTrips,
-  travelerDestinations;
+let pendingTrips = document.querySelector('.pending-trips');
+let pendingTripsArea = document.querySelector(".pending-trips-area");
+let pendingTripsText = document.querySelector(".pending-trips-text");
+
+// let homeButton = document.querySelector('.home');
+let upcomingTrips = document.querySelector(".upcoming-trips");
+let upcomingTripsArea = document.querySelector(".upcoming-trips-area");
+let upcomingTripsText = document.querySelector(".upcoming-trips-text");
+
+let pastTrips = document.querySelector(".past-trips");
+let pastTripsArea = document.querySelector(".past-trips-area");
+let pastTripsText = document.querySelector(".past-trips-text");
+
+let travelerApi;
+let destinationApi;
+let tripApi;
+let traveler;
 
 window.onload = getAllData();
+pendingTrips.addEventListener('click', displayPendingTrips);
+upcomingTrips.addEventListener('click', displayUpcomingTrips);
+pastTrips.addEventListener('click', displayPastTrips);
+// homeButton.addEventListener("click", returnHome);
 
 function getAllData() {
     travelerApi = new ApiCall('http://localhost:3001/api/v1/travelers', 'travelers');
@@ -78,8 +91,99 @@ function displayTrips(tripsList) {
     })
 }
 
+function displayPendingTrips() {
+  tripsArea.classList.add('hidden');
+  pendingTripsArea.classList.remove("hidden");
+  allTripsText.classList.add('hidden');
+  yearCost.classList.add('hidden');
+  let pendingTripsList = traveler.getPendingTrips();
 
- // tripInfo.forEach(trip => {
-        //     userTrip = new Trip(trip, destintation)                                                                                                          
-        //     allTrips.push(userTrip)
-        // })  
+   if (pendingTripsList.length === 0) {
+       pendingTripsText.innerText = 'You Have No Pending Trips!'
+   } else {
+       pendingTripsArea.innerHTML = '';
+       pendingTripsList.forEach(trip => {
+           let pendingTripsHTML = `
+           <div class='info-card'>
+               <div class="image-styling">
+                   <img src="${trip.destination.image}" alt="${trip.destination.alt}" class="trip-image">
+               </div>
+               <p id="${trip.destination.destination}-destination" class="trip-date">Destination: ${trip.destination.destination}</p>
+               <p id="${trip.date}-date" class="trip-date">Trip Date: ${trip.date}</p>
+               <p id="${trip.duration}-duration" class="trip-duration">Trip Duration: ${trip.duration}</p>
+               <p id="${trip.travelers}-travelers" class="trip-travelers">Number of Travelers: ${trip.travelers}</p>
+               <p id="${trip.status}-status" class="trip-status">Trip Status: ${trip.status}</p>
+           </div>
+       `;
+       pendingTripsArea.insertAdjacentHTML('beforeend', pendingTripsHTML)
+   })
+}}
+
+function displayUpcomingTrips() {
+  tripsArea.classList.add("hidden");
+  upcomingTripsArea.classList.remove("hidden");
+  allTripsText.classList.add("hidden");
+  yearCost.classList.add("hidden");
+  let upcomingTripsList = traveler.getUpcomingTrips();
+
+  if (upcomingTripsList.length === 0) {
+    upcomingTripsText.innerText = "You Have No Upcoming Trips!";
+  } else {
+    upcomingTripsArea.innerHTML = "";
+    upcomingTripsList.forEach((trip) => {
+      let upcomingTripsHTML = `
+           <div class='info-card'>
+               <div class="image-styling">
+                   <img src="${trip.destination.image}" alt="${trip.destination.alt}" class="trip-image">
+               </div>
+               <p id="${trip.destination.destination}-destination" class="trip-date">Destination: ${trip.destination.destination}</p>
+               <p id="${trip.date}-date" class="trip-date">Trip Date: ${trip.date}</p>
+               <p id="${trip.duration}-duration" class="trip-duration">Trip Duration: ${trip.duration}</p>
+               <p id="${trip.travelers}-travelers" class="trip-travelers">Number of Travelers: ${trip.travelers}</p>
+               <p id="${trip.status}-status" class="trip-status">Trip Status: ${trip.status}</p>
+           </div>
+       `;
+      upcomingTripsArea.insertAdjacentHTML("beforeend", upcomingTripsHTML);
+    });
+  }
+}
+
+function displayPastTrips() {
+  tripsArea.classList.add("hidden");
+  pastTripsArea.classList.remove("hidden");
+  allTripsText.classList.add("hidden");
+  yearCost.classList.add("hidden");
+  let pastTripsList = traveler.getPastTrips();
+
+  if (pastTripsList.length === 0) {
+    pastTripsText.innerText = "You Have No Upcoming Trips!";
+  } else {
+    pastTripsArea.innerHTML = "";
+    pastTripsList.forEach((trip) => {
+      let pastTripsHTML = `
+           <div class='info-card'>
+               <div class="image-styling">
+                   <img src="${trip.destination.image}" alt="${trip.destination.alt}" class="trip-image">
+               </div>
+               <p id="${trip.destination.destination}-destination" class="trip-date">Destination: ${trip.destination.destination}</p>
+               <p id="${trip.date}-date" class="trip-date">Trip Date: ${trip.date}</p>
+               <p id="${trip.duration}-duration" class="trip-duration">Trip Duration: ${trip.duration}</p>
+               <p id="${trip.travelers}-travelers" class="trip-travelers">Number of Travelers: ${trip.travelers}</p>
+               <p id="${trip.status}-status" class="trip-status">Trip Status: ${trip.status}</p>
+           </div>
+       `;
+      pastTripsArea.insertAdjacentHTML("beforeend", pastTripsHTML);
+    });
+  }
+}
+
+
+
+
+
+
+
+// function returnHome() {
+//   console.log("home");
+//   getAllData();
+// }
