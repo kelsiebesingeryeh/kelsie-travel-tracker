fc53f0238d159c899f145f1e71c82f4a1ff53be8
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-01-18 19:51:15 -0700

    Refactor domUpdates file

--- src/index.js
+++ src/index.js
@@ -4,43 +4,7 @@ import Traveler from './traveler';
 import ApiCall from './apiCalls';
 import domUpdates from './domUpdates';
 
-const tripsArea = document.querySelector('.travel-card-container');
-const yearCost = document.querySelector(".year-cost");
-const allTripsText = document.querySelector(".all-trips");
-const destinationsList = document.querySelector(".destinations-list");
-
-const pendingTrips = document.querySelector(".pending-trips");
-const pendingTripsArea = document.querySelector(".pending-trips-area");
-const pendingTripsText = document.querySelector(".pending-trips-text");
-
-let homeButton = document.querySelector('.home');
-let upcomingTrips = document.querySelector(".upcoming-trips");
-let upcomingTripsArea = document.querySelector(".upcoming-trips-area");
-let upcomingTripsText = document.querySelector(".upcoming-trips-text");
-
-let pastTrips = document.querySelector(".past-trips");
-let pastTripsArea = document.querySelector(".past-trips-area");
-let pastTripsText = document.querySelector(".past-trips-text");
-
-let currentTrips = document.querySelector(".current-trips");
-let currentTripsArea = document.querySelector(".current-trips-area");
-let currentTripsText = document.querySelector(".current-trips-text");
-
-let bookTravelButton = document.querySelector('.book-travel-button')
-const calculateTravelButton = document.querySelector(".calculate-cost-button");
-let durationInput = document.querySelector('.duration');
-let travelersInput = document.querySelector('.travelers');
-let startDate = document.querySelector(".date-picker");
-let estimatedTripCost = document.querySelector(".estimated-trip-cost");
-let loginSubmitButton = document.querySelector(".submit-button");
-let userView = document.querySelector(".user-view");
-let loginPage = document.querySelector(".login-page");
-let navbar = document.querySelector(".nav-bar");
-let usernameInput = document.querySelector('.username');
-let passwordInput = document.querySelector('.password');
-
-let hamburgerMenu = document.querySelector(".hamburger");
-let hamburgerMenuContent = document.querySelector(".hamburger-content");
+// GLOBAL VARIABLES
 
 let chosenTraveler;
 let travelerApi;
@@ -49,18 +13,62 @@ let tripApi;
 let destinationInfo;
 let travelerInfo;
 let tripInfo;
-let singleInfo;
+let currentUserInfo;
 let traveler;
-let trip;
 let singleTraveler;
 let newTrip;
 let chosenUserID;
 const baseURL = 'http://localhost:3001/api/v1';
 
+// QUERY SELECTORS
+
+const tripsArea = document.querySelector(".travel-card-container");
+const yearCost = document.querySelector(".year-cost");
+const allTripsText = document.querySelector(".all-trips");
+const destinationsList = document.querySelector(".destinations-list");
+
+const pendingTrips = document.querySelector(".pending-trips");
+const pendingTripsArea = document.querySelector(".pending-trips-area");
+const pendingTripsText = document.querySelector(".pending-trips-text");
+
+
+const upcomingTrips = document.querySelector(".upcoming-trips");
+const upcomingTripsArea = document.querySelector(".upcoming-trips-area");
+const upcomingTripsText = document.querySelector(".upcoming-trips-text");
+
+const pastTrips = document.querySelector(".past-trips");
+const pastTripsArea = document.querySelector(".past-trips-area");
+const pastTripsText = document.querySelector(".past-trips-text");
+
+const currentTrips = document.querySelector(".current-trips");
+const currentTripsArea = document.querySelector(".current-trips-area");
+const currentTripsText = document.querySelector(".current-trips-text");
+
+
+const homeButton = document.querySelector(".home");
+const bookTravelButton = document.querySelector(".book-travel-button");
+const calculateTravelButton = document.querySelector(".calculate-cost-button");
+const loginSubmitButton = document.querySelector(".submit-button");
+
+const durationInput = document.querySelector(".duration");
+const travelersInput = document.querySelector(".travelers");
+const startDate = document.querySelector(".date-picker");
+const estimatedTripCost = document.querySelector(".estimated-trip-cost");
+const userView = document.querySelector(".user-view");
+const loginPage = document.querySelector(".login-page");
+const navbar = document.querySelector(".nav-bar");
+const usernameInput = document.querySelector(".username");
+const passwordInput = document.querySelector(".password");
+
+const hamburgerMenu = document.querySelector(".hamburger");
+const hamburgerMenuContent = document.querySelector(".hamburger-content");
+
+// EVENT LISTENERS
+
 pendingTrips.addEventListener("click", displayPendingTrips);
 upcomingTrips.addEventListener("click", displayUpcomingTrips);
 pastTrips.addEventListener("click", displayPastTrips);
-currentTrips.addEventListener('click', displayCurrentTrips)
+currentTrips.addEventListener("click", displayCurrentTrips);
 calculateTravelButton.addEventListener('click', () => {
     displayEstimatedCosts(event);
     hide(calculateTravelButton);
@@ -90,45 +98,44 @@ function toggleHamburgerMenuDropdown() {
     hamburgerMenuContent.classList.toggle('hidden');
 }
 
-// homeButton.addEventListener("click", returnHome);
+homeButton.addEventListener("click", returnHome);
 
 function loginUser(event) {
     event.preventDefault()
     chosenUserID = usernameInput.value.split("").splice(8, 3).join("");
     if (usernameInput.value.slice(0, 8) === "traveler" && usernameInput.value.slice(8) > 0 && usernameInput.value.slice(8) <= 50 && passwordInput.value === 'traveler2020') {
-            getAllData();
+        getAllData();
     }
 }
 
 function getAllData() {
-  travelerApi = new ApiCall(`${baseURL}/travelers`, "travelers");
-  destinationApi = new ApiCall(`${baseURL}/destinations`, "destinations");
-  tripApi = new ApiCall(`${baseURL}/trips`, "trips");
-  singleTraveler = new ApiCall(`${baseURL}/travelers/${chosenUserID}`);
-  onLoad();
+    travelerApi = new ApiCall(`${baseURL}/travelers`, "travelers");
+    destinationApi = new ApiCall(`${baseURL}/destinations`, "destinations");
+    tripApi = new ApiCall(`${baseURL}/trips`, "trips");
+    singleTraveler = new ApiCall(`${baseURL}/travelers/${chosenUserID}`);
+    onLoad();
 }
 
 function onLoad() {
-  let travelerData = travelerApi.getRequest();
-  let destinationData = destinationApi.getRequest();
-  let tripData = tripApi.getRequest();
-  let singleData = singleTraveler.getSingleRequest();
+    let travelerData = travelerApi.getRequest();
+    let destinationData = destinationApi.getRequest();
+    let tripData = tripApi.getRequest();
+    let singleData = singleTraveler.getSingleRequest();
 
-  return Promise.all([travelerData, destinationData, tripData, singleData])
-    .then((data) => {
-      travelerInfo = data[0];
-      destinationInfo = data[1];
-      tripInfo = data[2];
-      singleInfo = data[3];
-      buildPage(singleInfo, tripInfo, destinationInfo, travelerInfo);
-    })
-    .catch((error) => console.log(error));
+    return Promise.all([travelerData, destinationData, tripData, singleData])
+        .then((data) => {
+            travelerInfo = data[0];
+            destinationInfo = data[1];
+            tripInfo = data[2];
+            currentUserInfo = data[3];
+            buildPage(currentUserInfo, tripInfo, destinationInfo, travelerInfo);
+        })
+        .catch((error) => console.log(error));
 }
 
-function createTravelerProfile(singleInfo, tripInfo,destinationInfo) {
-  fillDropdown();
-  traveler = new Traveler(singleInfo, tripInfo, destinationInfo);
-  console.log('user', singleInfo);
+function createTravelerProfile(currentUserInfo, tripInfo, destinationInfo) {
+    fillDropdown();
+    traveler = new Traveler(currentUserInfo, tripInfo, destinationInfo);
 }
 
 function updateNewTripBookings(event) {
@@ -137,15 +144,14 @@ function updateNewTripBookings(event) {
 }
 
 function fillDropdown() {
-
     let sortedDestinations = destinationInfo.sort((a, b) => {
         if (a.destination < b.destination) {
             return -1
         }
     })
     sortedDestinations.forEach((destination) => {
-      let destinationsOptions = `<option>${destination.destination}</option>`;
-      destinationsList.insertAdjacentHTML("beforeend", destinationsOptions);
+        let destinationsOptions = `<option>${destination.destination}</option>`;
+        destinationsList.insertAdjacentHTML("beforeend", destinationsOptions);
     });
 }
 
@@ -156,12 +162,10 @@ function clearTravelInputs() {
     destinationsList.selectedIndex = 0;
 }
 
-function buildPage(singleInfo, tripInfo, destinationInfo) {
-  createTravelerProfile(singleInfo, tripInfo, destinationInfo);
-  displayTrips(traveler);
-  yearCost.innerText = `Your 2020 trip cost is: $${traveler.calculateTotalSpent(
-    "2020"
-  )}`;
+function buildPage(currentUserInfo, tripInfo, destinationInfo) {
+    createTravelerProfile(currentUserInfo, tripInfo, destinationInfo);
+    domUpdates.displayTrips(traveler, tripsArea);
+    yearCost.innerText = `Your 2020 trip cost is: $${traveler.calculateTotalSpent("2020")}`;
 }
 
 function makeNewTrip() {
@@ -172,14 +176,14 @@ function makeNewTrip() {
     let uniqueID = getNewID();
 
     newTrip = {
-      id: uniqueID,
-      userID: singleInfo.id,
-      destinationID: destinationInputValue,
-      travelers: travelerInputValue,
-      date: newDateFormat,
-      duration: durationInputValue,
-      status: "pending",
-      suggestedActivities: [],
+        id: uniqueID,
+        userID: currentUserInfo.id,
+        destinationID: destinationInputValue,
+        travelers: travelerInputValue,
+        date: newDateFormat,
+        duration: durationInputValue,
+        status: "pending",
+        suggestedActivities: [],
     };
     return newTrip;
 }
@@ -206,158 +210,12 @@ function submitTripRequest() {
     newTripBooking.postRequest(postOption);
 }
 
-function displayTrips(tripsList) {
-    tripsArea.innerHTML = '';
-    tripsList.trips.forEach(trip => {
-        let tripsHTML = `
-                    <div class='info-card'>
-                    <div class="image-styling">
-                    <img src="${trip.destination.image}" alt="${trip.destination.alt}" class="trip-image">
-                    </div>
-                    <p id="${trip.destination.destination}-destination" class="trip-date">Destination: ${trip.destination.destination}</p>
-                    <p id="${trip.date}-date" class="trip-date">Trip Date: ${trip.date}</p>
-                    <p id="${trip.duration}-duration" class="trip-duration">Trip Duration: ${trip.duration}</p>
-                    <p id="${trip.travelers}-travelers" class="trip-travelers">Number of Travelers: ${trip.travelers}</p>
-                    <p id="${trip.status}-status" class="trip-status">Trip Status: ${trip.status}</p>
-                    </div>`;
-        tripsArea.insertAdjacentHTML('beforeend', tripsHTML)
-    })
-}
-
 function show(element) {
     element.classList.remove("hidden");
 }
 
 function hide(element) {
     element.classList.add("hidden");
-}
-
-function displayPendingTrips() {
-    hide(tripsArea)
-    show(pendingTripsArea);
-    hide(upcomingTripsArea);
-    hide(pastTripsArea);
-    hide(allTripsText)
-    hide(yearCost);
-    hide(currentTripsArea);
-    let pendingTripsList = traveler.getPendingTrips();
-
-    if (pendingTripsList.length === 0) {
-        pendingTripsText.innerText = 'You Have No Pending Trips!'
-    } else {
-        pendingTripsArea.innerHTML = '';
-        pendingTripsList.forEach(trip => {
-            let pendingTripsHTML = `
-           <div class='info-card'>
-               <div class="image-styling">
-                   <img src="${trip.destination.image}" alt="${trip.destination.alt}" class="trip-image">
-               </div>
-               <p id="${trip.destination.destination}-destination" class="trip-date">Destination: ${trip.destination.destination}</p>
-               <p id="${trip.date}-date" class="trip-date">Trip Date: ${trip.date}</p>
-               <p id="${trip.duration}-duration" class="trip-duration">Trip Duration: ${trip.duration}</p>
-               <p id="${trip.travelers}-travelers" class="trip-travelers">Number of Travelers: ${trip.travelers}</p>
-               <p id="${trip.status}-status" class="trip-status">Trip Status: ${trip.status}</p>
-           </div>
-       `;
-            pendingTripsArea.insertAdjacentHTML('beforeend', pendingTripsHTML)
-        })
-    }
-}
-
-function displayUpcomingTrips() {
-    hide(tripsArea);
-    show(upcomingTripsArea);
-    hide(pendingTripsArea);
-    hide(pastTripsArea);
-    hide(allTripsText);
-    hide(yearCost);
-    hide(currentTripsArea);
-    let upcomingTripsList = traveler.getUpcomingTrips();
-
-    if (upcomingTripsList.length === 0) {
-        upcomingTripsText.innerText = "You Have No Upcoming Trips!";
-    } else {
-        upcomingTripsArea.innerHTML = "";
-        upcomingTripsList.forEach((trip) => {
-            let upcomingTripsHTML = `
-           <div class='info-card'>
-               <div class="image-styling">
-                   <img src="${trip.destination.image}" alt="${trip.destination.alt}" class="trip-image">
-               </div>
-               <p id="${trip.destination.destination}-destination" class="trip-date">Destination: ${trip.destination.destination}</p>
-               <p id="${trip.date}-date" class="trip-date">Trip Date: ${trip.date}</p>
-               <p id="${trip.duration}-duration" class="trip-duration">Trip Duration: ${trip.duration}</p>
-               <p id="${trip.travelers}-travelers" class="trip-travelers">Number of Travelers: ${trip.travelers}</p>
-               <p id="${trip.status}-status" class="trip-status">Trip Status: ${trip.status}</p>
-           </div>
-       `;
-            upcomingTripsArea.insertAdjacentHTML("beforeend", upcomingTripsHTML);
-        });
-    }
-}
-
-function displayPastTrips() {
-    hide(tripsArea);
-    show(pastTripsArea);
-    hide(upcomingTripsArea);
-    hide(pendingTripsArea);
-    hide(allTripsText);
-    hide(yearCost);
-    hide(currentTripsArea);
-    let pastTripsList = traveler.getPastTrips();
-
-    if (pastTripsList.length === 0) {
-        pastTripsText.innerText = "You Have No Past Trips!";
-    } else {
-        pastTripsArea.innerHTML = "";
-        pastTripsList.forEach((trip) => {
-            let pastTripsHTML = `
-           <div class='info-card'>
-               <div class="image-styling">
-                   <img src="${trip.destination.image}" alt="${trip.destination.alt}" class="trip-image">
-               </div>
-               <p id="${trip.destination.destination}-destination" class="trip-date">Destination: ${trip.destination.destination}</p>
-               <p id="${trip.date}-date" class="trip-date">Trip Date: ${trip.date}</p>
-               <p id="${trip.duration}-duration" class="trip-duration">Trip Duration: ${trip.duration}</p>
-               <p id="${trip.travelers}-travelers" class="trip-travelers">Number of Travelers: ${trip.travelers}</p>
-               <p id="${trip.status}-status" class="trip-status">Trip Status: ${trip.status}</p>
-           </div>`;
-            pastTripsArea.insertAdjacentHTML("beforeend", pastTripsHTML);
-        });
-    }
-}
-
-function displayCurrentTrips() {
-    hide(tripsArea);
-    show(currentTripsArea)
-    hide(upcomingTripsArea);
-    hide(pendingTripsArea);
-    hide(pastTripsArea);
-    hide(allTripsText);
-    hide(yearCost);
-    pastTripsArea.classList.add("hidden");
-
-    let currentTripsList = traveler.getCurrentTrips();
-
-    if (currentTripsList.length === 0) {
-        currentTripsText.innerText = "You Have No Current Trips!";
-    } else {
-        currentTripsArea.innerHTML = "";
-        currentTripsList.forEach((trip) => {
-            let currentTripsHTML = `
-           <div class='info-card'>
-               <div class="image-styling">
-                   <img src="${trip.destination.image}" alt="${trip.destination.alt}" class="trip-image">
-               </div>
-               <p id="${trip.destination.destination}-destination" class="trip-date">Destination: ${trip.destination.destination}</p>
-               <p id="${trip.date}-date" class="trip-date">Trip Date: ${trip.date}</p>
-               <p id="${trip.duration}-duration" class="trip-duration">Trip Duration: ${trip.duration}</p>
-               <p id="${trip.travelers}-travelers" class="trip-travelers">Number of Travelers: ${trip.travelers}</p>
-               <p id="${trip.status}-status" class="trip-status">Trip Status: ${trip.status}</p>
-           </div>`;
-            currentTripsArea.insertAdjacentHTML("beforeend", currentTripsHTML);
-        });
-    }
 }
 
 function displayEstimatedCosts(event) {
@@ -374,16 +232,81 @@ function displayEstimatedCosts(event) {
     })
 }
 
+function displayPendingTrips() {
+    let pendingTripsList = traveler.getPendingTrips();
+    domUpdates.displayOtherTrips(pendingTripsList, pendingTripsArea, 'pendingHTML', pendingTripsText, "pending");
+    hide(tripsArea);
+    show(pendingTripsArea);
+    hide(upcomingTripsArea);
+    hide(pastTripsArea);
+    hide(allTripsText);
+    hide(yearCost);
+    hide(currentTripsArea);
+}
 
-// function returnHome(event) {
-// console.log('hi')
-//  show(userView);
-//  getAllData();
-//  hide(currentTripsArea);
-//  hide(pendingTripsArea);
-//  hide(upcomingTripsArea);
-//  hide(pastTripsArea);
-// }
+function displayUpcomingTrips() {
+    let upcomingTripsList = traveler.getUpcomingTrips();
+    domUpdates.displayOtherTrips(
+        upcomingTripsList,
+        upcomingTripsArea,
+        "upcomingHTML",
+        upcomingTripsText,
+        "Upcoming"
+    );
+    hide(tripsArea);
+    show(upcomingTripsArea);
+    hide(pendingTripsArea);
+    hide(pastTripsArea);
+    hide(allTripsText);
+    hide(yearCost);
+    hide(currentTripsArea);
+}
+
+function displayPastTrips() {
+    let pastTripsList = traveler.getPastTrips();
+    domUpdates.displayOtherTrips(
+        pastTripsList,
+        pastTripsArea,
+        "pastHTML",
+        pastTripsText,
+        "Previous"
+    );
+    hide(tripsArea);
+    show(pastTripsArea);
+    hide(pendingTripsArea);
+    hide(upcomingTripsArea);
+    hide(allTripsText);
+    hide(yearCost);
+    hide(currentTripsArea);
+}
+
+function displayCurrentTrips() {
+    let currentTripsList = traveler.getCurrentTrips();
+    domUpdates.displayOtherTrips(
+        currentTripsList,
+        currentTripsArea,
+        "currentHTML",
+        currentTripsText,
+        "Current"
+    );
+    hide(tripsArea);
+    show(currentTripsArea)
+    hide(upcomingTripsArea);
+    hide(pendingTripsArea);
+    hide(pastTripsArea);
+    hide(allTripsText);
+    hide(yearCost);
+}
+
+function returnHome(event) {
+console.log('hi')
+ show(userView);
+ getAllData();
+ hide(currentTripsArea);
+ hide(pendingTripsArea);
+ hide(upcomingTripsArea);
+ hide(pastTripsArea);
+}
 
 // .toLocaleString() - adds commas
 // .toLocaleString("en-US", {style: "currency", currency: "USD"})
